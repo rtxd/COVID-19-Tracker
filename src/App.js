e784efdbdbc16e1dee4eb4cac2f6c6c77dfc1d6f
Alex <alejandro.munoz@student.uts.edu.au>
2020-04-23 09:18:55 +1000

    Setup API and passing data through props of Cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import styles from "./App.module.css";
 import { Cards, Chart, CountryPicker } from "./components";
+import { fetchData } from "./api";
 
 class App extends React.Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
+
   render() {
+    const { data } = this.state;
+
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={this.state.data} />
         <CountryPicker />
         <Chart />
       </div>
